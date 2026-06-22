f7f46d3814040c270e2e74dcd30c9f45a2b88f49
Kk <s.keattisak@gmail.com>
2026-06-22 18:15:17 -0400

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -1,8 +1,15 @@
 import feedparser
 
-feed = feedparser.parse(
-    "https://feeds.bbci.co.uk/news/rss.xml"
-)
+weblist =(
+    {'url': "https://feeds.bbci.co.uk/news/rss.xml", "name":"BBC"},
+    {'url': 'https://www.thairath.co.th/rss/news', 'name': 'ไทยรัฐ'},
+    {'url': 'http://rssfeeds.sanook.com/rss/feeds/sanook/news.index.xml', 'name': 'สนุกดอทคอม'},
+    {'url': 'https://news.thaipbs.or.th/rss/news', 'name': 'thaipbs'},
+    {'url': 'https://www.prachachat.net/feed', 'name': 'ประชาชาติ'},
+    {'url': 'http://www.lokwannee.com/web2013/?cat=69&feed=rss2', 'name': 'โลกวันนี้'},
+    {'url': 'https://www.matichon.co.th/feed', 'name': 'มติชน'},
+    {'url': 'https://voicetv.co.th/rss', 'name': 'Voice TV'}
+    )
 
 html = """
 <html>
@@ -15,7 +22,9 @@ html = """
 
 <ul>
 """
-
+for web in weblist:
+    url = web["url"]
+    feed = feedparser.parse(url)
 for item in feed.entries[:10]:
 
     html += f"""
