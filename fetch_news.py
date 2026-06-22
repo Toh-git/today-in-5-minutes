cd39bee0b05c3f0a04c4a32f144b348da57dbc52
kk <s.keattisak@gmail.com>
2026-06-22 18:56:55 -0400

    Clean up

--- fetch_news.py
+++ fetch_news.py
@@ -1,18 +1,11 @@
 import feedparser
-<<<<<<< HEAD
-=======
->>>>>>> f7f46d3814040c270e2e74dcd30c9f45a2b88f49
 weblist =(
     {'url': "https://feeds.bbci.co.uk/news/rss.xml", "name":"BBC"},
     {'url': 'https://www.thairath.co.th/rss/news', 'name': 'ไทยรัฐ'},
     {'url': 'http://rssfeeds.sanook.com/rss/feeds/sanook/news.index.xml', 'name': 'สนุกดอทคอม'},
     {'url': 'https://news.thaipbs.or.th/rss/news', 'name': 'thaipbs'},
     {'url': 'https://www.prachachat.net/feed', 'name': 'ประชาชาติ'},
-<<<<<<< HEAD
     {'url': 'https://feeds.feedburner.com/prachatai', 'name': 'ประชาไทย'},
-=======
-    {'url': 'http://www.lokwannee.com/web2013/?cat=69&feed=rss2', 'name': 'โลกวันนี้'},
->>>>>>> f7f46d3814040c270e2e74dcd30c9f45a2b88f49
     {'url': 'https://www.matichon.co.th/feed', 'name': 'มติชน'},
     {'url': 'https://voicetv.co.th/rss', 'name': 'Voice TV'}
     )
@@ -28,7 +21,6 @@ html = """
 
 <ul>
 """
-<<<<<<< HEAD
 
 for web in weblist:
     url = web["url"]
@@ -47,20 +39,6 @@ for web in weblist:
             </a>
         </li>
         """
-=======
-for web in weblist:
-    url = web["url"]
-    feed = feedparser.parse(url)
-for item in feed.entries[:10]:
-
-    html += f"""
-    <li>
-        <a href="{item.link}">
-        {item.title}
-        </a>
-    </li>
-    """
->>>>>>> f7f46d3814040c270e2e74dcd30c9f45a2b88f49
 
 html += """
 </ul>
